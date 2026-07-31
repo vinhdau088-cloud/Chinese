f1579cdc5ac49852858e32eae421706a8d2c7a27
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 19:38:12 +0800

    Create page.tsx

---
+++ app/shadowing/page.tsx
@@ -0,0 +1,35 @@
+import ShadowingCard 
+from "@/components/shadowing/ShadowingCard";
+
+
+export default function ShadowingPage(){
+
+
+return(
+
+<main className="
+min-h-screen
+bg-gray-50
+p-8
+">
+
+
+<h1 className="
+text-4xl
+font-bold
+mb-8
+">
+
+🎧 Shadowing 中文
+
+</h1>
+
+
+<ShadowingCard/>
+
+
+</main>
+
+)
+
+}
