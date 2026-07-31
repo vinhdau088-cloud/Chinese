e96ea681098f03f776731157863f0f70d700ba16
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 20:38:13 +0800

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,108 +1,18 @@
-export default function AdminPage(){
-
-import UserTable 
-from "@/components/admin/UserTable";
-return(
-
-<main className="
-p-8
-">
+import UserTable from "@/components/admin/UserTable";
 
 
-<h1 className="
-text-4xl
-font-bold
-mb-8
-">
+export default function AdminPage() {
 
-⚙️ Admin Dashboard
+  return (
+    <main className="min-h-screen p-6">
 
-</h1>
+      <h1 className="text-3xl font-bold mb-6">
+        Admin Dashboard
+      </h1>
 
-<UserTable/>
+      <UserTable />
 
-<div className="
-grid
-md:grid-cols-3
-gap-5
-">
-
-
-<div className="
-bg-white
-rounded-3xl
-shadow
-p-6
-">
-
-👥
-
-<p>
-
-Users
-
-</p>
-
-</div>
-
-
-
-<div className="
-bg-white
-rounded-3xl
-shadow
-p-6
-">
-
-📚
-
-<p>
-
-Vocabulary
-
-</p>
-
-</div>
-
-<a
-href="/admin/vocabulary"
-className="
-bg-white
-rounded-3xl
-shadow
-p-6
-block
-"
->
-
-📚 Manage Vocabulary
-
-</a>
-
-<div className="
-bg-white
-rounded-3xl
-shadow
-p-6
-">
-
-🎧
-
-<p>
-
-Audio
-
-</p>
-
-</div>
-
-
-
-</div>
-
-
-</main>
-
-)
+    </main>
+  );
 
 }
