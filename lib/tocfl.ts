3bb1819ef6bd180c05963759cbaca0d15e3b05b8
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 19:27:44 +0800

    Update tocfl.ts

--- lib/tocfl.ts
+++ lib/tocfl.ts
@@ -1,9 +1,19 @@
 import a1 from "@/data/tocfl/a1.json";
+import a2 from "@/data/tocfl/a2.json";
+import b1 from "@/data/tocfl/b1.json";
+import b2 from "@/data/tocfl/b2.json";
+import c1 from "@/data/tocfl/c1.json";
+import c2 from "@/data/tocfl/c2.json";
 
 
-const tocfl:any={
+const data:any = {
 
-A1:a1
+A1:a1,
+A2:a2,
+B1:b1,
+B2:b2,
+C1:c1,
+C2:c2
 
 };
 
@@ -12,6 +22,6 @@ export function getTOCFLWords(
 level:string
 ){
 
-return tocfl[level] || [];
+return data[level] || [];
 
 }
