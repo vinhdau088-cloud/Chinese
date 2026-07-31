f7c09e87a9fc4d1ebc6ddd7324b47c1ad67ec71f
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 18:59:23 +0800

    Create vocabulary.ts

---
+++ types/vocabulary.ts
@@ -0,0 +1,23 @@
+export interface Vocabulary {
+
+id:number;
+
+word:string;
+
+pinyin:string;
+
+zhuyin?:string;
+
+meaning:string;
+
+type?:string;
+
+level:string;
+
+example?:string;
+
+translation?:string;
+
+audio?:string;
+
+}
