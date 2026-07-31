4c34a8f805ece7a2c16ff3322c3cd370f7e81736
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 20:14:43 +0800

    Create route.ts

---
+++ app/api/admin/vocabulary/route.ts
@@ -0,0 +1,23 @@
+import {NextResponse} from "next/server";
+
+
+export async function POST(
+req:Request
+){
+
+
+const body =
+await req.json();
+
+
+
+return NextResponse.json({
+
+success:true,
+
+vocabulary:body
+
+});
+
+
+}
