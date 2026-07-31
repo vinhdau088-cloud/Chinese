426e30ba09e20d328084360ab9b5cf27858d3977
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 18:06:44 +0800

    Create page.tsx

---
+++ app/flashcard/page.tsx
@@ -0,0 +1,19 @@
+import Flashcard from "@/components/flashcard/Flashcard";
+
+export default function FlashcardPage(){
+
+return (
+
+<main className="
+min-h-screen
+bg-gray-50
+p-6
+">
+
+<Flashcard />
+
+</main>
+
+)
+
+}
