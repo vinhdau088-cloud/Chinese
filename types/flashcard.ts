35b71b87ab6b905087e797ee7d03fe47da5e154d
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 18:12:46 +0800

    Create flashcard.ts

---
+++ types/flashcard.ts
@@ -0,0 +1,21 @@
+export interface FlashcardType{
+
+id:number;
+
+hanzi:string;
+
+pinyin:string;
+
+zhuyin?:string;
+
+meaning:string;
+
+example?:string;
+
+translation?:string;
+
+audio?:string;
+
+level:string;
+
+}
