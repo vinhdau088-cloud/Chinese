0b4107f0bb21258a96ddcfc26c8bb29a2a26fb8f
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 20:01:44 +0800

    Create page.tsx

---
+++ app/achievement/page.tsx
@@ -0,0 +1,36 @@
+import Badge from "@/components/profile/Badge";
+
+
+export default function Achievement(){
+
+
+return(
+
+<main className="p-8">
+
+
+<h1 className="
+text-4xl
+font-bold
+mb-8
+">
+
+🏅 Thành tích
+
+</h1>
+
+
+<div className="flex gap-3">
+
+<Badge name="Học 100 từ"/>
+
+<Badge name="7 ngày liên tục"/>
+
+</div>
+
+
+</main>
+
+)
+
+}
