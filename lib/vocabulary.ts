42018542f5d692c6c437e763b433480431ea4d16
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 21:22:34 +0800

    Update vocabulary.ts

--- lib/vocabulary.ts
+++ lib/vocabulary.ts
@@ -8,46 +8,75 @@ import hsk6 from "@/data/hsk/hsk6.json";
 
 const data:any = {
 
-HSK1:hsk1,
-HSK2:hsk2,
-HSK3:hsk3,
-HSK4:hsk4,
-HSK5:hsk5,
-HSK6:hsk6
+  HSK1:hsk1,
+  HSK2:hsk2,
+  HSK3:hsk3,
+  HSK4:hsk4,
+  HSK5:hsk5,
+  HSK6:hsk6
 
 };
 
 
-export function getHSKWords(
-level:string
-){
 
-return data[level] || [];
+export function getHSKWords(level:string){
+
+  return data[level] || [];
 
 }
 
 
 
-export function searchWord(
-keyword:string
-){
-
-const all=[
-...hsk1,
-...hsk2,
-...hsk3,
-...hsk4,
-...hsk5,
-...hsk6
-];
+export function searchWord(keyword:string){
 
 
-return all.filter(
- item =>
- item.word?.includes(keyword) ||
- item.hanzi?.includes(keyword) ||
- item.pinyin?.includes(keyword) ||
- item.meaning?.includes(keyword)
-);
+  const all:any[] = [
+
+    ...hsk1,
+    ...hsk2,
+    ...hsk3,
+    ...hsk4,
+    ...hsk5,
+    ...hsk6
+
+  ];
+
+
+
+  return all.filter((item:any)=>{
+
+
+    const word =
+      item.word ||
+      item.hanzi ||
+      "";
+
+    const pinyin =
+      item.pinyin ||
+      "";
+
+    const meaning =
+      item.meaning ||
+      "";
+
+
+
+    return (
+
+      word.includes(keyword)
+
+      ||
+
+      pinyin.includes(keyword)
+
+      ||
+
+      meaning.includes(keyword)
+
+    );
+
+
+  });
+
 
 }
