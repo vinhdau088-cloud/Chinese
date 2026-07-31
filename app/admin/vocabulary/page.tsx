d505d0b358a92d8985bda4a16fb240212182693f
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 20:15:32 +0800

    Create page.tsx

---
+++ app/admin/vocabulary/page.tsx
@@ -0,0 +1,33 @@
+import AddVocabulary
+from "@/components/admin/AddVocabulary";
+
+
+export default function VocabularyAdmin(){
+
+
+return(
+
+<main className="
+p-8
+">
+
+
+<h1 className="
+text-4xl
+font-bold
+mb-8
+">
+
+📚 Vocabulary Manager
+
+</h1>
+
+
+<AddVocabulary/>
+
+
+</main>
+
+)
+
+}
