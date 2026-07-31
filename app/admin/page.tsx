80dacb73d5d3bbab57257c2b51d262962f610a07
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 20:11:33 +0800

    Create page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,94 @@
+export default function AdminPage(){
+
+
+return(
+
+<main className="
+p-8
+">
+
+
+<h1 className="
+text-4xl
+font-bold
+mb-8
+">
+
+⚙️ Admin Dashboard
+
+</h1>
+
+
+
+<div className="
+grid
+md:grid-cols-3
+gap-5
+">
+
+
+<div className="
+bg-white
+rounded-3xl
+shadow
+p-6
+">
+
+👥
+
+<p>
+
+Users
+
+</p>
+
+</div>
+
+
+
+<div className="
+bg-white
+rounded-3xl
+shadow
+p-6
+">
+
+📚
+
+<p>
+
+Vocabulary
+
+</p>
+
+</div>
+
+
+
+<div className="
+bg-white
+rounded-3xl
+shadow
+p-6
+">
+
+🎧
+
+<p>
+
+Audio
+
+</p>
+
+</div>
+
+
+
+</div>
+
+
+</main>
+
+)
+
+}
