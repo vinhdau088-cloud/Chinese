f7eb4260b783036f5d9b91c795c2737153c28c25
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 19:37:52 +0800

    Create ShadowingCard.tsx

---
+++ components/shadowing/ShadowingCard.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import AudioControl from "./AudioControl";
+import SpeedControl from "./SpeedControl";
+
+
+export default function ShadowingCard(){
+
+
+function speed(value:number){
+
+console.log(
+"Speed:",
+value
+);
+
+}
+
+
+
+return(
+
+<div className="
+bg-white
+rounded-3xl
+shadow
+p-8
+max-w-xl
+">
+
+
+<h1 className="
+text-5xl
+font-bold
+text-purple-600
+">
+
+你好
+
+</h1>
+
+
+<p className="mt-3">
+nǐ hǎo
+</p>
+
+
+<p>
+ㄋㄧˇ ㄏㄠˇ
+</p>
+
+
+<p className="
+mt-5
+">
+
+Xin chào
+
+</p>
+
+
+<AudioControl
+
+src="/audio/hsk/hsk1/nihao.mp3"
+
+/>
+
+
+<SpeedControl
+
+change={speed}
+
+/>
+
+
+</div>
+
+)
+
+}
