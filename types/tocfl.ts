855db3c08527d42ad6d488482e4d61ac2fc31964
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-31 19:25:49 +0800

    Create tocfl.ts

---
+++ types/tocfl.ts
@@ -0,0 +1,23 @@
+export interface TOCFLWord{
+
+id:number;
+
+word:string;
+
+traditional:string;
+
+pinyin:string;
+
+zhuyin:string;
+
+meaning:string;
+
+level:string;
+
+example:string;
+
+translation:string;
+
+audio?:string;
+
+}
